3453977912d4231171469b6878d7eccd9926f9c8
Michael Macherey <macherey@gmx.net>
2016-06-19 17:21:04 +0000

    add johnny-five and send accelerometer data

--- index.js
+++ index.js
@@ -1,8 +1,84 @@
+"use strict";
 // Todo: Setup Meteor App install DDP and establish first connetion.
 // and config https://www.npmjs.com/package/config
 
+// ngrok: Secure tunnels to localhost
+// ngrok start with: ./ngrok http 3000
+// and change line 27
+
 var raspi = require('raspi-io');
-var five = require('johnny-five');
+var five = require('johnny-five'),board, nunchuk;
+var DDPClient = require("ddp");
+
+var ddpclient = new DDPClient({
+  // All properties optional, defaults shown
+//  host : "localhost",
+//  port : 3000,
+  ssl  : false,
+  autoReconnect : true,
+  autoReconnectTimer : 500,
+  maintainCollections : true,
+  ddpVersion : "1",  // ["1", "pre2", "pre1"] available,
+  // uses the sockJs protocol to create the connection
+  // this still uses websockets, but allows to get the benefits
+  // from projects like meteorhacks:cluster
+  // (load balancing and service discovery)
+  // do not use `path` option when you are using useSockJs
+  useSockJs: false,
+  // Use a full url instead of a set of `host`, `port` and `ssl`
+  // do not set `useSockJs` option if `url` is used
+//  url: 'wss://example.com/websocket'
+  url: 'ws://192cebf5.ngrok.io/websocket'
+});
+
+/*
+ * Connect to the Meteor Server
+ */
+ddpclient.connect(function(error, wasReconnect) {
+  // If autoReconnect is true, this callback will be invoked each time
+  // a server connection is re-established
+  if (error) {
+    console.log("DDP connection error!");
+    return;
+  }
+
+  if (wasReconnect) {
+    console.log("Reestablishment of a connection.");
+  }
+
+  console.log("connected!");
+
+    /*
+     * Subscribe to a Meteor Collection
+     */
+    ddpclient.subscribe(
+      "tasks",                  // name of Meteor Publish function to subscribe to
+      [],                       // any parameters used by the Publish function
+      function () {             // callback when the subscription is complete
+        console.log("tasks complete:");
+        console.log(ddpclient.collections.tasks);
+      }
+    );
+
+    //ddpclient.call('tasks.insert',["Hello Moon!"]);
+
+    ddpclient.subscribe(
+      "accelerometer",                  // name of Meteor Publish function to subscribe to
+      [],                       // any parameters used by the Publish function
+      function () {             // callback when the subscription is complete
+        console.log("accelerometer complete:");
+        console.log(ddpclient.collections.accelerometer);
+      }
+    );
+
+
+
+    ddpclient.call('tasks.insert',["Hello Moon task!"]);
+
+
+  });
+
+
 var board = new five.Board({
   io: new raspi()
 });
@@ -45,6 +121,7 @@ board.on('ready', function() {
       event.target[event.axis],
       event.axis, event.direction
     );
+
   });
 
   // "change", "axischange" (accelerometer)
@@ -58,6 +135,20 @@ board.on('ready', function() {
       event.target[event.axis],
       event.axis, event.direction
     );
+
+    ddpclient.call('accelerometer.insert',[{
+      value : event.target[event.axis],
+      axis : event.axis,
+      direction : event.direction
+    }], (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('success send accelerometer data')
+        // success!
+      }
+    });
+
   });
 
   // "down"
